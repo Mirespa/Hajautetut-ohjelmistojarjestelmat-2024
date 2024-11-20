cab8a4fcbc8151795751d2c5212ba2a44758a258
Miro Prusila <mprussa02@gmail.com>
2024-11-20 20:53:35 +0200

    Demo3 tehtävä 2 valmis

--- demo3/src/main/java/fi/utu/tech/assignment2/Server.java
+++ demo3/src/main/java/fi/utu/tech/assignment2/Server.java
@@ -1,9 +1,27 @@
 package fi.utu.tech.assignment2;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // TODO: Kopioi edellisen tehtäväsi vastaus tähän pohjalle
+        ServerSocket serverSocket = new ServerSocket(2000);
+        System.out.println("Running");
+        
+        Socket commSocket = serverSocket.accept();
+        System.out.println("Connected");
+
+        var iStream = commSocket.getInputStream();
+        byte[] received = iStream.readAllBytes();
+
+        String reveivedMesssage = new String(received, "utf-8");
+        System.out.println("Message: " + reveivedMesssage);
+
+        commSocket.close();
+        serverSocket.close();
     }
 
 }
